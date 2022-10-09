dcdd6198c2e925e1a00841eb155f02cc42951c24
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2022-10-09 17:20:40 -0600

    replaced '=' with '+=' when calculating total wait time so value is calculated correctly

--- gym_cityflow/envs/cityflow_env.py
+++ gym_cityflow/envs/cityflow_env.py
@@ -91,7 +91,7 @@ class CityFlowEnv(gym.Env):
         self.current_step += 1
 
         # add current wait time to total
-        self.total_wait_time = sum(self.eng.get_lane_waiting_vehicle_count().values())
+        self.total_wait_time += sum(self.eng.get_lane_waiting_vehicle_count().values())
 
         # An episode is done once we have simulated the number of steps defined in episode_steps
         terminated = self.episode_steps == self.current_step
