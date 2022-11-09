bae7587ae76fee9272a3e04d4cf85c6b28fbf5a2
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2022-11-09 13:59:30 -0700

    removed +1 when creating Discrete space for 'steps_in_phase' in observation space dictionary

--- gym_cityflow/envs/cityflow_env.py
+++ gym_cityflow/envs/cityflow_env.py
@@ -73,7 +73,7 @@ class CityFlowEnv(gym.Env):
         observation_space_dict = self.eng.get_lane_waiting_vehicle_count()
         for key in observation_space_dict:
             observation_space_dict[key] = spaces.Discrete(self.metadata["max_waiting"])
-        observation_space_dict["steps_in_phase"] = spaces.Discrete(self.max_phase_time + 1)
+        observation_space_dict["steps_in_phase"] = spaces.Discrete(self.max_phase_time)
         self.observation_space = spaces.Dict(observation_space_dict)
 
         # Verify and set render mode
